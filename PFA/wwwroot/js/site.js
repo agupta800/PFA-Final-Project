70ac5820ddd50aeec841deefd9073bb64e84a43b
agupta800 <awadheshg577@gmail.com>
2024-02-10 20:11:12 +0545

    Eye toggle added

--- PFA/wwwroot/js/site.js
+++ PFA/wwwroot/js/site.js
@@ -112,3 +112,38 @@ $('#CustomImg img').on('mouseleave', function () {
 $('#CloseBtn').on('click', function () {
     $('#ImgModel').hide();
 });
+
+
+//Show Password
+
+    //function togglePasswordVisibility() {
+    //        var passwordInput = document.getElementById('password');
+    //var eyeIcon = document.getElementById('eyeIcon');
+
+    //if (passwordInput.type === 'password') {
+    //    passwordInput.type = 'text';
+    //eyeIcon.classList.remove('bi-eye');
+    //eyeIcon.classList.add('bi-eye-slash');
+    //        } else {
+    //    passwordInput.type = 'password';
+    //eyeIcon.classList.remove('bi-eye-slash');
+    //eyeIcon.classList.add('bi-eye');
+    //        }
+    //    }
+
+
+    function togglePasswordVisibility(passwordId, eyeIconId) {
+        var passwordInput = document.getElementById(passwordId);
+    var eyeIcon = document.getElementById(eyeIconId);
+
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+    eyeIcon.classList.remove('bi-eye');
+    eyeIcon.classList.add('bi-eye-slash');
+        } else {
+        passwordInput.type = 'password';
+    eyeIcon.classList.remove('bi-eye-slash');
+    eyeIcon.classList.add('bi-eye');
+        }
+    }
+
